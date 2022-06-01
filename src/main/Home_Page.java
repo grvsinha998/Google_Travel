8be8041afc98945b768410a8475b162e6ec4c398
Gaurav Sinha <grvsinha998@gmail.com>
2022-06-01 21:58:46 +0530

    Day 1

--- src/main/Home_Page.java
+++ src/main/Home_Page.java
@@ -1,29 +1,24 @@
 package main;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.asserts.SoftAssert;
 
 import javax.net.ssl.HttpsURLConnection;
 import java.io.IOException;
-import java.net.ProtocolException;
 import java.net.URL;
 import java.time.Duration;
 import java.util.List;
-import java.util.Set;
-import java.util.concurrent.TimeUnit;
 
 public class Home_Page {
-    public static void main(String[] args) throws IOException, InterruptedException {
+    public static void main(String[] args) throws IOException {
         System.setProperty("webdriver.chrome.driver", "/Users/gauravsinha/Downloads/chromedriver");
         WebDriver driver = new ChromeDriver();
-        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
         driver.manage().window().maximize();
         driver.get("https://www.google.com/travel/");
 
@@ -50,20 +45,17 @@ public class Home_Page {
             wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='heading'][text()='Travel']")));
         }
     }
-    public static void Check_all_Options(WebDriver driver) throws InterruptedException, IOException {
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(30));
-        WebElement SideBar = driver.findElement(By.xpath("//nav[@class='rlGvde']"));
-        Actions axn = new Actions(driver);
-
+    public static void Check_all_Options(WebDriver driver) throws IOException {
         List<WebElement> Travel_urls = driver.findElements(By.xpath("//nav[@class='rlGvde']//a"));
+        SoftAssert sa = new SoftAssert();
         for (WebElement Travel_url: Travel_urls) {
             String url = Travel_url.getAttribute("href");
             HttpsURLConnection Travel_conn = (HttpsURLConnection) new URL(url).openConnection();
             Travel_conn.setRequestMethod("HEAD");
             Travel_conn.connect();
             int Resp_Code = Travel_conn.getResponseCode();
-
-            Assert.assertEquals(Resp_Code,200);
+            sa.assertEquals(Resp_Code, 200, url + " is working.");
         }
-        }
+        sa.assertAll();
     }
+}
